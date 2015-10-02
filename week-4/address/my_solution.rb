7c6f54d663e2cbe8cae79cf5c49626b87ea55d3c
Doug Tsui <dbtsui@gmail.com>
2015-10-01 20:01:15 -0700

    Finish up 4.3

--- week-4/address/my_solution.rb
+++ week-4/address/my_solution.rb
@@ -6,19 +6,7 @@
 # Your Solution Below
 
 def make_address (street, city, state, zip)
-	puts "What is your street address?"
-	street=gets.chomp
 
-	puts "What city do you live in?"
-	city=gets.chomp
-
-	puts "What state do you live in?"
-	state=gets.chomp
-
-	puts "What is your zip?"
-	zip=gets.chomp
-
-	print "You live at " + street ",in the beautiful city of" + city ", " + state ". Your
-	zip is " + zip "." 
-
+	puts "\"You live at " + street + ", in the beautiful city of " + city +
+		 ", " + state + ". Your zip is " + zip + ".\"" 
 end
